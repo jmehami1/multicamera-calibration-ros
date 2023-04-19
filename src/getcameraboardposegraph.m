71d2f7017bcb51ff685c03c60b547c5cf1de6402
jmehami1 <jasprabhjit.mehami@student.uts.edu.au>
2023-04-19 20:05:18 +1000

    added parameters as arguments with default values set

--- src/getcameraboardposegraph.m
+++ src/getcameraboardposegraph.m
@@ -1,48 +1,63 @@
-function [nodes,edges] = getcameraboardposegraph(timeStamps, cameraIDs, poses)
+function [nodes,edges] = getcameraboardposegraph(timeStamps, cameraIDs, poses, timeDiffThreshold, timeBetweenBoard, minNumBoardsGroup, camOriginID)
 % Get board nodes using timeStamps from estimated camera extrinsics. It
 % works by assuming that estimated board poses within a window of time
 % are captured when the board is in the exact same pose. For each
-% board node, there will exist only one unique pose w.r.t. each camera.
+% board node, there will exist only one unique pose w.r.t. each camera. A
+% board node is only useful if it has atleast 2 camera node edges. The
+% edges with each board node can vary.
+% A board pose is an estimated extrinsic w.r.t. a single camera. A board
+% node is the board pose w.r.t. the origin camera at a single time-step.
 % INPUTS:
 %       timeStamps - timestamps of estimated poses
 %       cameraIDs - w.r.t. camera ID of estimated poses
 %       poses - poses as [x,y,z,qw,qx,qy,qz]
+%       timeDiffThreshold - Maximum time (in seconds) where measured board
+%       poses are considered to be the same board node.
+%       timeBetweenBoard - Maximum time (in seconds) between boards. This 
+%       ensures board nodes are not to close together to avoid overlapping.
+%       camOriginID - Camera ID thats considered the origin
 % OUTPUTS:
-%       nodes - board node ID and its pose (initalised to be at the origin). First node ID will always be the last camera
-%               ID + 1
-%       edges - 
-%           
+%       nodes - board node ID and its pose (initalised to be at the origin). 
+%       First board node ID will always be the last camera node ID + 1
+%       edges - poses of a board w.r.t. a single camera. 
+%
+% Author: Jasprabhjit Mehami, 16/04/2023
 
+arguments
+    timeStamps;
+    cameraIDs;
+    poses;
+    timeDiffThreshold = 0.1;
+    timeBetweenBoard = 0.2;
+    minNumBoardsGroup = 2;
+    camOriginID = 1;
+end
 
-%Maximum time difference threshold where a measured relative pose of
-%the board w.r.t to a camera is considered to be the same pose of the
-%board
-timeDiffThreshold = 0.1;
-timeBetweenBoard = 0.2;
-minNumBoardsGroup = 2;
-camOriginID = 1;
 %sort timestamps
-
 [timeStamps, sortedIdxs] = sort(timeStamps);
 cameraIDs = double(cameraIDs(sortedIdxs));
 maxCameraIDs = max(cameraIDs);
 
 numPoses = length(timeStamps);
-boardIdxGroupedCell = cell(1, numPoses);
-camIdGroupedCell = cell(1, numPoses);
+boardIdxGroupedCell = cell(1, numPoses); % Board IDs assigned to each group
+camIdGroupedCell = cell(1, numPoses); %cam IDs assigned to each group
 numGroups = 1;
-numPosesGroup = zeros(1,numPoses);
+numPosesGroup = zeros(1,numPoses); % Number of poses for each group
 
+%initialising variables with first pose
 boardIdxGroupedCell(1) = {sortedIdxs(1)};
 camIdGroupedCell(1) = {cameraIDs(1)};
 groupStartTimeStamp = timeStamps(1);
 numPosesGroup(1) = 1;
 
+% Go through each pose and check if within timeDiffThreshold to assign it 
+% to the current group, else start a new group.
+
 for i = 2:numPoses
     curTimeStamp = timeStamps(i);
     curIdx = sortedIdxs(i);
     curCamID = cameraIDs(i);
-    
+
     if abs(curTimeStamp - groupStartTimeStamp) < timeDiffThreshold
         groupIdxs = boardIdxGroupedCell{numGroups};
         groupIdxs = [groupIdxs, curIdx];
@@ -52,7 +67,6 @@ for i = 2:numPoses
         camIds = [camIds, curCamID];
         camIdGroupedCell(numGroups) = {camIds};
         numPosesGroup(numGroups) = numPosesGroup(numGroups) + 1;
-
     elseif abs(groupStartTimeStamp - curTimeStamp) > timeBetweenBoard
         numGroups = numGroups + 1;
         boardIdxGroupedCell(numGroups) = {curIdx};
@@ -103,8 +117,8 @@ nodes(:,5) = 1;
 nodes(1,1) = 1;
 
 for i = 2:maxCameraIDs
-%     randTF = -1 + 2*rand(1,7);
-%     randTF = [zeros(1,6), 1];
+    %     randTF = -1 + 2*rand(1,7);
+    %     randTF = [zeros(1,6), 1];
     nodes(i,1) = i;
 end
 
